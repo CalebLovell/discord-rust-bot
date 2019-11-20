accb9c227b37ae26556ffa762ed3a7ff1d4d1d92
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-11-20 02:12:53 -0700

    refactor command system and add commands for setting and viewing main event timers

--- bot.js
+++ bot.js
@@ -3,83 +3,48 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
+const commandPrefix = '!';
 
-let timeNow = new Date().toLocaleTimeString('en-US');
-let lastHeliTime
-let lastCargoTime
-let lastBradleyTime
+let lastHeliTime;
+let lastCargoTime;
+let lastBradleyTime;
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Mr. Naked is at your command.');
 });
 
 client.on('message', message => {
-	if (message.content === '!heli') {
-		try {
-			lastHeliTime = timeNow;
-			message.channel.send(`The heli spawn time has been set to: ${lastHeliTime}. To check this time in the future, type the command ?heli.`);
-		} catch (error) {
-			console.log(`Something went wrong: ${error}`)
-		}
+	const commandArgs = message.content.slice(commandPrefix.length).split(/ +/);
+	const command = commandArgs.shift().toLowerCase();
+
+	const timeNow = new Date().toLocaleTimeString('en-US');
+
+	if (!message.content.startsWith(commandPrefix) || message.author.bot) return;
+
+	// Commands
+	else if (command === 'setheli') {
+		lastHeliTime = timeNow;
+		message.channel.send(`The heli spawn time has been set to: ${lastHeliTime}. To check this time later, type the question ?heli.`);
 	}
-	console.log(message.content);
-});
-
-client.on('message', message => {
-	if (message.content === '?heli') {
-		try {
-			message.channel.send(`The heli was last recorded up at ${lastHeliTime}.`);
-		} catch (error) {
-			console.log(`Something went wrong: ${error}`)
-		}
+	else if (command === 'setcargo') {
+		lastCargoTime = timeNow;
+		message.channel.send(`The cargo spawn time has been set to: ${lastCargoTime}. To check this time later, type the question ?cargo.`);
 	}
-	console.log(message.content);
-});
-
-client.on('message', message => {
-	if (message.content === '!cargo' || message.content === '!boat' || message.content === '!ship') {
-		try {
-			lastCargoTime = timeNow;
-			message.channel.send(`The cargo ship spawn time has been set to: ${lastCargoTime}. To check this time in the future, type the command ?cargo.`);
-		} catch (error) {
-			console.log(`Something went wrong: ${error}`)
-		}
+	else if (command === 'setbradley') {
+		lastBradleyTime = timeNow;
+		message.channel.send(`The bradley spawn time has been set to: ${lastBradleyTime}. To check this time later, type the question ?bradley.`);
 	}
-	console.log(message.content);
+
+	// Questions
+	else if (command === 'heli') {
+		message.channel.send(`The heli was last up at ${lastHeliTime}.`);
+	}
+	else if (command === 'cargo') {
+		message.channel.send(`The heli was last up at ${lastCargoTime}.`);
+	}
+	else if (command === 'bradley') {
+		message.channel.send(`The heli was last up at ${lastBradleyTime}.`);
+	}
 });
-
-// client.on('message', message => {
-// 	if (message.content === '?cargo' || '?boat' || '!ship') {
-// 		try {
-// 			message.channel.send(`The cargo ship was last recorded up at ${lastCargoTime}.`);
-// 		} catch (error) {
-// 			console.log(`Something went wrong: ${error}`)
-// 		}
-// 	}
-// 	console.log(message.content);
-// });
-
-// client.on('message', message => {
-// 	if (message.content === '!bradley' || '!brady' || '!apc' || '!tank') {
-// 		try {
-// 			lastBradleyTime = timeNow;
-// 			message.channel.send(`The Bradley APC spawn time has been set to: ${lastBradleyTime}. To check this time in the future, type the command ?bradley.`);
-// 		} catch (error) {
-// 			console.log(`Something went wrong: ${error}`)
-// 		}
-// 	}
-// 	console.log(message.content);
-// });
-
-// client.on('message', message => {
-// 	if (message.content === '?bradley' || '?brady' || '?apc' || '?tank') {
-// 		try {
-// 			message.channel.send(`The Bradley APC was last recorded up at ${lastBradleyTime}.`);
-// 		} catch (error) {
-// 			console.log(`Something went wrong: ${error}`)
-// 		}
-// 	}
-// 	console.log(message.content);
-// });
 
 client.login(TOKEN);
